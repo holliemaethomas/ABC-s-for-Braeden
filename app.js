d7d6f582d2d158cf47f47107d5a5043721a25ac0
holliemaethomas <holliemaethomas85@gmail.com>
2018-07-28 17:33:39 -0700

    add random

--- app.js
+++ app.js
@@ -2,16 +2,36 @@
 // write the letters to the DOM//
 //add button for next//
 var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var counter = 1;
+var displayNumber = 1;
 
 let getLetters = document.getElementById('letterBox');
 getLetters.innerHTML = alphabet[0];
-
+let washDone = false;
 document.getElementById('showNext').addEventListener('click', function (){
-  getLetters.innerHTML = alphabet[counter];
-  counter ++;
-  console.log(alphabet);
+  getLetters.innerHTML = alphabet[displayNumber];
+  displayNumber ++;
+  console.log(displayNumber);
+  // console.log(alphabet);
+  if(displayNumber === 26){
+    washDone = true;
+  }
+  
+  if(washDone === true){
+    var randomNumber = makeRandom();
+    displayNumber = randomNumber;
+    console.log(displayNumber);
+  }
+
+
 });
+
+function makeRandom() {
+  let random = Math.random();
+  let otherNumber = alphabet.length;
+  let randomNumber = random * otherNumber;
+  console.log(random, otherNumber, randomNumber);
+  return Math.floor(randomNumber);
+}
 
 
 
